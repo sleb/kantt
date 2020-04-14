ee515b413b6f2583c1cc8f80660b40856158eb99
sleb <scott.g.lebaron@gmail.com>
2020-04-13 22:58:25 -0700

    add zircon dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import Versions.zircon
+
 plugins {
     kotlin("jvm") version "1.3.70"
     application
@@ -10,6 +12,8 @@ repositories {
 dependencies {
     implementation(platform(kotlin("bom")))
     implementation(kotlin("stdlib-jdk8"))
+    implementation("org.hexworks.zircon:zircon.core:$zircon")
+    implementation("org.hexworks.zircon:zircon.jvm.swing:$zircon")
 
     testImplementation(kotlin("kotlin-test"))
     testImplementation(kotlin("test-junit"))
---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,3 @@
+object Versions {
+    const val zircon = "2020.1.0-RELEASE"
+}
